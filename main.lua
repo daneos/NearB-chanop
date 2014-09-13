d1502dca306c260cd46cedae11d23ce7beeec28b
daneos <daneos@daneos.com>
2014-09-13 23:15:10 +0200

    working test bot

--- main.lua
+++ main.lua
@@ -2,19 +2,21 @@
 -- Copyright (C) 2014 daneos.
 -- Released under the MIT license. See LICENSE for details
 
-require "lib.LuaIRC"
+require "irc.init" --WTF??
 local sleep = require "socket".sleep
 
 local s = irc.new{nick = "chanop"}
 
-s:hook("OnChat", function(user, channel, message)
-print(("[%s] %s: %s"):format(channel, user.nick, message))
-end)
+s:hook("OnChat",
+	function(user, channel, message)
+		print(("[%s] %s: %s"):format(channel, user.nick, message))
+	end
+)
 
-s:connect("127.0.0.1")
+s:connect("wiktor.ml")
 s:join("#test")
 
 while true do
-s:think()
-sleep(0.5)
+	s:think()
+	sleep(0.5)
 end
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+lua5.1 main.lua
