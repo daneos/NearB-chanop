61d7f004cfdebb4d4e9916aa0949cca985a69989
daneos <daneos@daneos.com>
2014-09-14 01:49:46 +0200

    gowno

--- main.lua
+++ main.lua
@@ -2,20 +2,32 @@
 -- Copyright (C) 2014 daneos.
 -- Released under the MIT license. See LICENSE for details
 
-require "irc.init" --WTF??
+require "irc.init" -- WTF??
 local sleep = require "socket".sleep
 
-local s = irc.new{nick = "chanop"}
+s = irc.new{nick = "chanop"}
 
 s:hook("OnChat",
 	function(user, channel, message)
-		print(("[%s] %s: %s"):format(channel, user.nick, message))
+		print(".")
+		s:sendNotice("#global", "["..channel.."] "..user.nick..": "..message)
+		if channel == "#announce" then
+				local key,lat1,lat2,ns,lon1,lon2,we,action = message:match("%[(%x+)%@(%d+)%.(%d+)(%u)(%d+)%.(%d+)(%u)%](%u+)")
+				s:sendNotice("#debug", "["..channel.."] "..user.nick..": KEY="..key.." LAT="..lat1.."."..lat2..ns.." LON="..lon1.."."..lon2..we.." ACTION="..action)
+			end
+		end
+		if channel == "#debug" and message == "QUIT" then
+			debug("Shutting down...")
+			exit(0)
+		end
 	end
 )
 
 s:connect("wiktor.ml")
 s:join("#test")
-
+s:join("#global")
+s:join("#announce")
+s:join("#debug")
 while true do
 	s:think()
 	sleep(0.5)
