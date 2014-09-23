84548ee49f3018f081273861bf8bd57e03c29556
daneos <daneos@daneos.com>
2014-09-23 20:01:09 +0200

    changed protocol

--- hooks.lua
+++ hooks.lua
@@ -14,24 +14,61 @@ local function auth(nick, key, pass)
 	return true
 end
 
+local function channel_exists(channel)
+	for _,chan in ipairs(bot.chans) do
+		if chan == channel then return true end
+	end
+	return false
+end
+
 local auth_queue = {}
+local registered = {}
 
 local hooks = {
 	["OnChat"] = function(user, channel, message)
 		bot:sendChat(bot.chans.global, "["..channel.."] "..user.nick..": "..message)
 		
 		if channel == bot.chans.announce then
-			local key,lat,lon,action = message:match("^%[(%x+)%@(%d+%.%d+%u)(%d+%.%d+%u)%](%u+)$")
-			if not key or not lat or not lon or not action then
+			local key,action,param = message:match("^(%x+):(%u+)%s?%[?(.-)%]?$")
+			if not key or not action then
 				bot:sendChat(bot.chans.announce, user.nick..":SYNTAX ERROR")
 				bot:debug("Syntax error on "..channel.." from "..user.nick)
 				return
 			end 
-			bot:debug("["..channel.."] "..user.nick..": KEY="..key.." LAT="..lat.." LON="..lon.." ACTION="..action)
+			bot:debug("["..channel.."] "..user.nick..": KEY="..key.." ACTION="..action)
+
 			if action == "CONNECT" then
-				auth_queue[user.nick] = {key=key,lat=lat,lon=lon}
+				auth_queue[user.nick] = key
 				bot:sendChat(user.nick, key..":AUTHORIZATION REQUIRED")
-			end		
+			elseif action == "UPDATE" then
+				local lat,lon = param:match("^(%d+%.%d+%u)(%d+%.%d+%u)$")
+				if not lat or not lon then
+					bot:sendChat(bot.chans.announce, user.nick..":SYNTAX ERROR")
+					bot:debug("Syntax error on "..channel.." from "..user.nick)
+					return
+				end
+				local data = registered[user.nick]
+				if data then
+					local loc = "#"..locator(lat,lon)
+					if data.channel then
+						if data.channel == loc then
+							bot:sendChat(bot.chans.announce, user.nick..":NO CHANGES")
+							return
+						end
+					end
+					if not channel_exists(loc) then
+						bot:join(loc)
+						table.insert(bot.chans, loc)
+						-- set channel "invite only" and send invite
+						bot:setMode({target=loc, add="i"})
+					end
+					bot:send("INVITE "..user.nick.." "..loc)
+					registered[user.nick].channel = loc
+				else
+					bot:sendChat(bot.chans.announce, user.nick..":NOT REGISTERED")
+					bot:debug("Not registered update from "..user.nick)
+				end
+			end
 		end
 		
 		if channel == bot.chans.debug then
@@ -56,22 +93,19 @@ local hooks = {
 
 		-- messages on /query
 		if channel == bot.userinfo.nick then
-			local data = auth_queue[user.nick]
-			if data then
+			local ckey = auth_queue[user.nick]
+			if ckey then
 				local key,pass = message:match("^(%x+)%:(.+)$")
 				if not key or not pass then
 					bot:sendChat(user.nick, user.nick..":SYNTAX ERROR")
 					bot:debug("Syntax error on /query from "..user.nick)
-				elseif key ~= data.key then
+				elseif key ~= ckey then
 					bot:sendChat(user.nick, key..":INVALID KEY")
-					bot:debug("Invalid key ["..key.."] from "..user.nick.." (should be ["..data.key.."])")
+					bot:debug("Invalid key ["..key.."] from "..user.nick.." (should be ["..ckey.."])")
 				elseif auth(user.nick, key, pass) then
-					loc = "#"..locator(data.lat, data.lon)
-					bot:join(loc)
-					table.insert(bot.chans, loc)
-					-- set channel "invite only" and send invite
-					bot:setMode({target=loc, add="i"})
-					bot:send("INVITE "..user.nick.." "..loc)
+					bot:sendChat(user.nick, key..":AUTH OK")
+					bot:debug("Authorized "..user.nick..", key "..key)
+					registered[user.nick] = {key=key}
 				else
 					bot:sendChat(user.nick, user.nick..":"..key.." UNAUTHORIZED")
 					bot:debug("Authorization error for "..user.nick..", key "..key)
